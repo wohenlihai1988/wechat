59ea1a3ae1591b0f59ca327ecc6774df823513f5
刘大侠 <wohenlihai1988@163.com>
2018-04-14 18:31:32 +0800

    add offical test logic

--- handle.py
+++ handle.py
@@ -7,24 +7,17 @@ import web
 class Handle(object):
     def GET(self):
         try:
-            data = web.input()
-            if len(data) == 0:
-                return "hello, this is handle view"
-            signature = data.signature
-            timestamp = data.timestamp
-            nonce = data.nonce
-            echostr = data.echostr
-            token = "helloworld" #请按照公众平台官网\基本配置中信息填写
-
-            list = [token, timestamp, nonce]
-            list.sort()
-            sha1 = hashlib.sha1()
-            map(sha1.update, list)
-            hashcode = sha1.hexdigest()
-            print "handle/GET func: hashcode, signature: ", hashcode, signature
-            if hashcode == signature:
-                return echostr
+            webData = web.data()
+            print 'handle post webdata is ', webData
+            recMsg = receive.parse_xml(webData)
+            if isinstance(recMsg, receive.Msg) and recmsg.MstType == 'text':
+                toUser = recMsg.FromUserName
+                fromUser = recMsg.ToUserName
+                content = 'test'
+                replyMsg = reply.TextMsg(toUser, fromUser, content)
+                return replyMsg.send()
             else:
-                return ""
+                print 'do nothing for now'
+                return 'success' 
         except Exception, Argument:
             return Argument
---
+++ receive.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import xml.etree.ElementTree as ET
+def parase_xml(web_data):
+    if len(web_data) == 0:
+        return None
+    xmlData = ET.fromstring(web_data)
+    msg_type = xmlData.find('MsgType').text
+    if msg_type == 'text':
+        return TextMsg(xmlData)
+    elif msg_type == 'image':
+        return ImageMsg(xmlData)
+
+class Msg(object):
+    def __init__ (self, xmlData):
+        self.ToUserName = xmlData.find('ToUserName').text
+        self.FromUserName = xmlData.find('FromUserName').text
+        self.CreateTime = xmlData.find('CreateTime').text
+        self.MsgType = xmlData.find('MsgType').text
+        self.MsgId = xmlData.find('MsgId').text
+
+class TextMsg(Msg):
+    def __init__ (self, xmlData):
+        Msg.__init__(self, xmlData)
+        self.PicUrl = xmlData.find('PicUrl').text
+        self.MediaId = xmlData.find('MediaId').text
